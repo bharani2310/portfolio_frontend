c75b1211dfa06331dd56773753e148e3dd4c57ea
bharani2310 <124676061+bharani2310@users.noreply.github.com>
2025-02-08 15:21:24 +0530

    minor changes

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
             <Link activeClass='active' to='skills-section' spy={true}  smooth={true} offset={-75} duration={500} className='menuListItem'>About</Link>
             <Link activeClass='active' to='tech-section' spy={true}   smooth={true} offset={-80} duration={500} className='menuListItem'>Skills</Link>
             <Link activeClass='active' to='works' spy={true} smooth={true} offset={-70}  duration={500} className='menuListItem'>Projects</Link>       
-            <Link activeClass='active' to='contactPage-section' spy={true}  smooth={true} offset={0}  duration={500} className='menuListItem'>Contact Me</Link> 
+            <Link activeClass='active' to='contactPage-section' spy={true}  smooth={true} offset={20}  duration={500} className='menuListItem'>Contact Me</Link> 
         </div>
 
 
