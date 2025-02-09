e0e5a77354ae6b546461d69b75b3d2995f162a4a
bharani2310 <124676061+bharani2310@users.noreply.github.com>
2025-02-09 20:49:43 +0530

    Url changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
 import Intro from "./components/Introduction/intro";
@@ -15,7 +16,7 @@ function App() {
       <Router>
       <Navbar /> {/* Keep Navbar outside Routes to display on all pages */}
         <Routes>
-          <Route path="/portfolio_frontend" element={<>
+          <Route path="/" element={<>
             <Intro />
             <Skills />
             <Tech/>
