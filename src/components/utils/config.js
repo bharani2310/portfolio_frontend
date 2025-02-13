91f2f786cf36d9d6235dae62471db00d8b5c2ee8
bharani2310 <124676061+bharani2310@users.noreply.github.com>
2025-02-13 21:25:55 +0530

    changes

--- src/components/utils/config.js
+++ src/components/utils/config.js
@@ -1,2 +1,2 @@
-export const  BASE_URL = 'https://backend-fsob.onrender.com/portfolio';
+export const  BASE_URL = 'http://34.82.0.222:4000/portfolio';
 // export const  BASE_URL = 'http://192.168.31.153:4000/portfolio';
