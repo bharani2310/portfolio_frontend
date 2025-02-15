e28ecf04d9ecdbf1043fb2de621558924e229c1e
bharani2310 <124676061+bharani2310@users.noreply.github.com>
2025-02-15 12:40:31 +0530

    Url change

--- src/components/utils/config.js
+++ src/components/utils/config.js
@@ -1,2 +1,3 @@
-export const  BASE_URL = 'https://34.82.0.222:4000/portfolio';
+export const  BASE_URL = 'https://api.bharanidharan.in/portfolio';
+// export const  BASE_URL = 'https://34.82.0.222:4000/portfolio';
 // export const  BASE_URL = 'http://192.168.31.153:4000/portfolio';
