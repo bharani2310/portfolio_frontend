bbd382593c44f8d1b23e5092dbc30e2b0a56458c
bharani2310 <124676061+bharani2310@users.noreply.github.com>
2025-04-18 12:31:56 +0530

    Minor url update

--- src/components/utils/config.js
+++ src/components/utils/config.js
@@ -1,3 +1,3 @@
-export const  BASE_URL = 'https://api.bharanidharan.in/portfolio';
+// export const  BASE_URL = 'https://api.bharanidharan.in/portfolio';
 // export const  BASE_URL = 'https://34.82.0.222:4000/portfolio';
-// export const  BASE_URL = 'http://192.168.1.10:4000/portfolio';
+export const  BASE_URL = 'https://backend-nl7u.onrender.com';
